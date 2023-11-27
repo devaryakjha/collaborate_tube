5daea7184502a9a328598b9504a9e15686e31584
devaryakjha <dev.jha.arya@gmail.com>
2023-11-27 16:04:10 +0530

    Update healthcheck endpoint URL

--- api/user-service/index.py
+++ api/user-service/index.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, FastAPI
 
-app = FastAPI(docs_url="/api/user-service/docs", redoc_url="/api/user-service/redoc", openapi_url="/api/user-service/openapi.json")
+app = FastAPI(docs_url="/api/user-service/docs/", redoc_url="/api/user-service/redoc/", openapi_url="/api/user-service/openapi.json")
 
 prefix_router = APIRouter(prefix="/api/user-service")
 
-@prefix_router.get("/")
-def read_root():
+@prefix_router.get("/healthchecker/")
+def healthcheck():
     return {"service": "user-service", "status": "OK", "version": "1.0.0"}
 
 app.include_router(prefix_router)
