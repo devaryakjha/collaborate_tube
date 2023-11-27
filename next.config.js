1f5d0c3813b2aa9a128b074c740115a3d5b26327
devaryakjha <dev.jha.arya@gmail.com>
2023-11-28 02:38:44 +0530

    Remove user-service API routes and update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  trailingSlash: true,
-  rewrites: async () => [
-    {
-      source: "/api/:path*",
-      destination:
-        process.env.NODE_ENV === "development"
-          ? "http://127.0.0.1:8000/api/:path*"
-          : "/api/",
-    },
-  ],
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
