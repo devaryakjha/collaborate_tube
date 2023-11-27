4f60bfbe26344b6e3d13229cbf1da3ffb7578164
devaryakjha <dev.jha.arya@gmail.com>
2023-11-27 15:52:39 +0530

    Remove trailing slashes and update API routes

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
   rewrites: async () => [
-    {
-      source: "/api/user-service/",
-      destination:
-        process.env.NODE_ENV === "development"
-          ? "http://127.0.0.1:8000/api/user-service/"
-          : "/api/",
-    },
-    {
-      source: "/api/user-service/:path*",
-      destination:
-        process.env.NODE_ENV === "development"
-          ? "http://127.0.0.1:8000/api/user-service/:path*"
-          : "/api/",
-    },
     {
       source: "/api/:path*",
       destination:
