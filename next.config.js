c03a7a45cf42a4acff643549c88221b4fe00a3a2
devaryakjha <dev.jha.arya@gmail.com>
2023-11-27 15:25:03 +0530

    Add API route for user-service

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  rewrites: [
+    {
+      source: "/api/:path*",
+      destination:
+        process.env.NODE_ENV === "development"
+          ? "http://127.0.0.1:8000/api/:path*"
+          : "/api/",
+    },
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
